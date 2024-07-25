963e32d2508822f863b4573f851ef2ff2fe57b7e
hiep957 <hicharmie1@gmail.com>
2024-07-25 19:06:05 +0700

    s

--- components/Form.tsx
+++ components/Form.tsx
@@ -235,7 +235,7 @@ const Form = () => {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/login" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
